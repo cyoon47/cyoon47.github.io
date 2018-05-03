1dcb604ec839dc4baadd70ad69b1e714226ab7c0
Chang Yoon Lee <cyoon47@hotmail.com>
2018-05-03 23:54:48 +0900

    airplane test

--- midi.js
+++ midi.js
@@ -1,4 +1,43 @@
 window.onload = function () {
+
+  var BPM = 60;
+
+  var notes = [];
+  notes.push({rest:0, pitch:"A3", type:0.75});
+  notes.push({rest:0, pitch:"G3", type:0.25});
+  notes.push({rest:0, pitch:"F3", type:0.5});
+  notes.push({rest:0, pitch:"G3", type:0.5});
+
+  notes.push({rest:0, pitch:"A3", type:0.5});
+  notes.push({rest:0, pitch:"A3", type:0.5});
+  notes.push({rest:0, pitch:"A3", type:1.0});
+
+  notes.push({rest:0, pitch:"G3", type:0.5});
+  notes.push({rest:0, pitch:"G3", type:0.5});
+  notes.push({rest:0, pitch:"G3", type:1.0});
+
+  notes.push({rest:0, pitch:"A3", type:0.5});
+  notes.push({rest:0, pitch:"A3", type:0.5});
+  notes.push({rest:0, pitch:"A3", type:1.0});
+
+  notes.push({rest:0, pitch:"A3", type:0.75});
+  notes.push({rest:0, pitch:"G3", type:0.25});
+  notes.push({rest:0, pitch:"F3", type:0.5});
+  notes.push({rest:0, pitch:"G3", type:0.5});
+
+  notes.push({rest:0, pitch:"A3", type:0.5});
+  notes.push({rest:0, pitch:"A3", type:0.5});
+  notes.push({rest:0, pitch:"A3", type:1.0});
+
+  notes.push({rest:0, pitch:"G3", type:0.5});
+  notes.push({rest:0, pitch:"G3", type:0.5});
+  notes.push({rest:0, pitch:"A3", type:0.75});
+  notes.push({rest:0, pitch:"G3", type:0.25});
+
+  notes.push({rest:0, pitch:"F3", type:2.0});
+
+  console.log(notes);
+
 	MIDI.loadPlugin({
 		soundfontUrl: "./soundfont/",
 		instrument: "acoustic_grand_piano",
@@ -12,16 +51,31 @@ window.onload = function () {
 	});
 
 	document.getElementById("playBtn").onclick = function(){
-		var delay = 0; // play one note every quarter second
-		var note = 60; // the MIDI note
-		var velocity = 127; // how hard the note hits
-		// play the note
-		MIDI.setVolume(0, 127);
-		for(var i = 0; i < 8; i++){
-			MIDI.noteOn(0, note+i, velocity, delay);
-			delay += 0.5;
-			MIDI.noteOff(0, note+i, delay);
-		}
-    console.log(MIDI.keyToNote["C3"]);
+    playMIDI(notes, 120);
 	};
+}
+
+/*
+note data: rest, pitch, type
+pitch: C3, D4...
+type: {4.0, 3.0, 2.0, 1.5, 1.0, 0.75, 0.5, 0.375, 0.25} (온음표 -> 16분음표)
+*/
+var playNote = function(note, delay, length){
+  MIDI.noteOn(0, note, 127, delay);
+  MIDI.noteOff(0, note, delay+length);
+};
+
+var playMIDI = function(notes, bpm){
+  var delay = MIDI.getContext().currentTime;
+  var qLength = 60.0 / bpm;
+  console.log(qLength);
+  MIDI.setVolume(0, 127);
+  notes.foreach(function(note){
+    if(note.rest == 1){
+      delay += qLength * note.type;
+    }
+    else{
+      playNote(MIDI.keyToNote[note.pitch], delay, qLength * note.type);
+    }
+  })
 }
