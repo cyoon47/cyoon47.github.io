07e8f5797b80be2c3fd29aca12dcd7c15effafef
Chang Yoon Lee <cyoon47@hotmail.com>
2018-05-03 23:00:52 +0900

    separate js, add jquery

---
+++ midi.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+		onprogress: function(state, progress) {
+			console.log(state, progress);
+		},
+		onsuccess: function() {
+			console.log("Loaded");
+			document.getElementById("playBtn").disabled = false;
+		}
+	});
+
+	document.getElementById("playBtn").onclick = function(){
+		var delay = 0; // play one note every quarter second
+		var note = 50; // the MIDI note
+		var velocity = 127; // how hard the note hits
+		// play the note
+		MIDI.setVolume(0, 127);
+		for(var i = 0; i < 8; i++){
+			MIDI.noteOn(0, note+i, velocity, delay);
+			delay += 0.5;
+			MIDI.noteOff(0, note+i, delay);
+		}
+	};
